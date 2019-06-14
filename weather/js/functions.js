75a9cc3bbbf408a7ae0ab3b8f455fd2275a70636
Garrett Bond <gbond3@byui.edu>
2019-06-14 13:47:58 -0600

    First activity started and starting second activity

---
+++ weather/js/functions.js
@@ -0,0 +1,18 @@
+/* *************************************
+*  Weather Site JavaScript Functions
+*
+*  Instructions: Comment each new function 
+*  and code within a function to clearly
+*  identify what it does and any special
+*  information that may need additional
+*  explanation. Don't forget!
+*
+************************************* */
+//test
+
+console.log('My javascript is being read.');
+
+//Calculate the Windchill
+function buildWC(speed, temp){
+
+}
