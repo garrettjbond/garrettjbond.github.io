b9bf91d872686b844fed01d6ffc20358d02c036d
Garrett Bond <gbond3@byui.edu>
2019-06-30 21:41:01 -0600

    check 2 weatherURL

--- weather/js/localdata.js
+++ weather/js/localdata.js
@@ -1,13 +1,11 @@
 "use strict";
 
 
-
-
 let pageNav = document.getElementById('page-nav');
 let statusContainer = document.getElementById('status');
 let contentContainer = document.getElementById('main-content');
 
-let weatherURL = "./weather.json";
+let weatherURL = "./js/weather.json";
 
 fetchData(weatherURL);
 
