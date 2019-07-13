07511b2175f6630287007908d50831f41208bde0
Garrett Bond <gbond3@byui.edu>
2019-07-13 16:34:32 -0600

    CSS and JS with JSON Nav Setup

--- acme/js/functions.js
+++ acme/js/functions.js
@@ -1,1 +1,37 @@
-console.log("test");
+'use strict';
+
+let navListItems = "../js/acme.json";
+
+console.log("test2");
+fetchData(navListItems);
+
+console.log("test3");
+function fetchData(navListItems) {
+  
+  fetch(navListItems)
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new ERROR('Network response was not OK.');
+    })
+    .then(function (data) {
+        console.log(data.navList);
+        findNav(data.navList);
+
+    });
+}
+
+function findNav(navList){
+
+    let naviListItems = '<li> Home </li>';
+    // Build the remaining list items using a for loop
+    navList.map(function(nav, idx){
+        naviListItems += '<li>' + nav + '</li>';
+        console.log(nav);
+    })
+       
+    // let list = document.getElementById("navBarList");
+    // list.innerHTML = buildHourlyData(nextHour, hourlyTemp);
+    
+}
