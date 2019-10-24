e8ce79db8a18f0cc16d4f45ef23a0abd74379891
garrettjbond <48851694+garrettjbond@users.noreply.github.com>
2019-10-24 09:00:17 -0600

    Javascript Connection Full | Setup of CSS

--- cit261/javascript/toDo.js
+++ cit261/javascript/toDo.js
@@ -29,9 +29,18 @@ list.addEventListener('click', function(ev){
 }, false);
 
 //Create a new list item when clicking on the add button
-function newElement(){
-    var li = document.createElement('li');
-    var inputValue = document.getElementById("myInput").value="";
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("myList").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+  
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
     span.className = "close";
@@ -44,4 +53,4 @@ function newElement(){
         div.style.display = "none";
       }
     }
-}
+  }
