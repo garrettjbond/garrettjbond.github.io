e599ea66c12a19fcf4f602bfc4486a1d0581f82a
garrettjbond <48851694+garrettjbond@users.noreply.github.com>
2019-10-24 10:05:07 -0600

    Minor CSS Changes

--- cit261/javascript/toDo.js
+++ cit261/javascript/toDo.js
@@ -22,10 +22,10 @@ for(i = 0; i < close.length; i++){
 
 //Add a checked symbol when clicking on a list item
 var list = document.querySelector('ul');
-list.addEventListener('click', function(ev){
-    if(ev.target.tagName === "li"){
-        ev.target.classList.toggle("checked");
-    }
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
 }, false);
 
 //Create a new list item when clicking on the add button
