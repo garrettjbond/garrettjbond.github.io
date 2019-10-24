390c65bc640c46dd50f8d9f499c5f6d929b47c13
garrettjbond <48851694+garrettjbond@users.noreply.github.com>
2019-10-23 18:09:52 -0600

    javascript changes

--- cit261/javascript/toDo.js
+++ cit261/javascript/toDo.js
@@ -10,9 +10,38 @@ for(i = 0; i < myNodelist.length; i++){
 }
 
 //Click on a close button to hide the current list item
-var close = document.getElementsByClassName
+var close = document.getElementsByClassName("close");
+var i;
+for(i = 0; i < close.length; i++){
+    close[i].onclick = function(){
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
 
 
 //Add a checked symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev){
+    if(ev.target.tagName === "li"){
+        ev.target.classList.toggle("checked");
+    }
+}, false);
 
 //Create a new list item when clicking on the add button
+function newElement(){
+    var li = document.createElement('li');
+    var inputValue = document.getElementById("myInput").value="";
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+}
