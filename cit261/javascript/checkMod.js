56f26af47a6c505a801b3b3f87b0e355ad40a557
garrettjbond <48851694+garrettjbond@users.noreply.github.com>
2019-10-26 15:28:30 -0600

    Modularization

---
+++ cit261/javascript/checkMod.js
@@ -0,0 +1,15 @@
+
+//Create Checked symbol for every onclicked task
+
+function createCheck(document){
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+}
+
+export{
+    createCheck
+}
