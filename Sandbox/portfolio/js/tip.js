b36993ebffb781ac44fefa02584d67b6e1be35e0
garrettjbond <48851694+garrettjbond@users.noreply.github.com>
2019-11-06 15:58:09 -0700

    Completion of  Day One  of Hundred

--- Sandbox/portfolio/js/tip.js
+++ Sandbox/portfolio/js/tip.js
@@ -1,1 +1,15 @@
 console.log("test connection");
+
+function calculateTip(){
+    //gets the user input for the pre total and assigns it to a variable.
+var preTotal = document.getElementById("befTotal").value;
+console.log(preTotal);
+//gets the user input for the quality of service and assigns it to a variable.
+var service = document.getElementById("befTotal").value;
+console.log(quaService);
+
+var tip = preTotal * (service / 100);
+     
+alert(tip);
+
+}
