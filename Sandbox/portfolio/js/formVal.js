a544254ed1f70e83508a31aeca294f30bfeba546
garrettjbond <48851694+garrettjbond@users.noreply.github.com>
2019-11-08 15:28:27 -0700

    name form prep

--- Sandbox/portfolio/js/formVal.js
+++ Sandbox/portfolio/js/formVal.js
@@ -1,1 +1,15 @@
 console.log("Test Connection")
+
+function validateInfo(){
+    //set variable
+    
+
+    //How to access data from an input
+    var formData = document.forms["formVal"]["fName"].value;
+
+    console.log(formData);
+
+    if(formData == ""){
+        alert("please fill with content");
+    }
+}
