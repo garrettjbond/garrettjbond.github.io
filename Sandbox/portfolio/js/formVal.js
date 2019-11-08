7f40cf007d305fb81ebacd64e85b4ca374866f50
garrettjbond <48851694+garrettjbond@users.noreply.github.com>
2019-11-08 15:45:15 -0700

    Form Validator Functionality Start

--- Sandbox/portfolio/js/formVal.js
+++ Sandbox/portfolio/js/formVal.js
@@ -1,15 +1,17 @@
 console.log("Test Connection")
 
-function validateInfo(){
+function accountCreation(){
     //set variable
     
 
     //How to access data from an input
-    var formData = document.forms["formVal"]["fName"].value;
+    // var formData = document.forms["formVal"]["fName"].value;
 
-    console.log(formData);
+    // console.log(formData);
 
-    if(formData == ""){
-        alert("please fill with content");
-    }
+    // if(formData == ""){
+    //     alert("please fill with content");
+    // }
+
+    alert("Account Created!");
 }
