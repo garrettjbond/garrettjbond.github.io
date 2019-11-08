fa809dcacb2c222afbea1c72c37d17ab4829e930
garrettjbond <48851694+garrettjbond@users.noreply.github.com>
2019-11-08 14:41:20 -0700

    Fixed the decimal point calculation issue

--- Sandbox/portfolio/js/tip.js
+++ Sandbox/portfolio/js/tip.js
@@ -2,20 +2,22 @@ console.log("test connection");
 
 function calculateTip(){
 //gets the user input for the pre total and assigns it to a variable.
-var preTotal = parseInt(document.getElementById("befTotal").value);
+var preTotal = parseFloat(document.getElementById("befTotal").value);
 //changes the input to be the desired decimal place.
 var pTotal = preTotal.toFixed(4);
 
 console.log(pTotal)
 
-console.log(preTotal);
-//gets the user input for the quality of service and assigns it to a variable.
-var service = parseInt(document.getElementById("quaService").value);
-var serv = service.toFixed(4);
 
+//gets the user input for the quality of service and assigns it to a variable.
+var service = parseFloat(document.getElementById("quaService").value);
+var serv = service.toFixed(4);
+console.log(serv);
 
 
 var tip = pTotal * (serv / 100.00);
+0
+tip.toFixed(2);
 
 console.log(tip);
 
