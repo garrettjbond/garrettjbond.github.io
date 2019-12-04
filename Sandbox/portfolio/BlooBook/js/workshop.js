c902daa08aa2748874ee3b6cc088508fa0b4698e
garrettjbond <48851694+garrettjbond@users.noreply.github.com>
2019-12-03 21:58:25 -0700

    Nav Bar Fix

---
+++ Sandbox/portfolio/BlooBook/js/workshop.js
@@ -0,0 +1,9 @@
+function toggleSideBar(x){
+    x.classList.toggle("change");
+  }
+  
+  function openNav(){ document.getElementById("asideWrapper").style.width = "350px";
+  }
+  
+  function closeNav(){  document.getElementById("asideWrapper").style.width = "0px";
+  }
