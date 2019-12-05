1c8b2a6f91722e499c28efb0e335444f73b9e0cf
garrettjbond <48851694+garrettjbond@users.noreply.github.com>
2019-12-04 22:20:06 -0700

    Almost finished Full web changes

--- Sandbox/portfolio/BlooBook/js/workshop.js
+++ Sandbox/portfolio/BlooBook/js/workshop.js
@@ -2,7 +2,10 @@ function toggleSideBar(x){
     x.classList.toggle("change");
   }
   
-  function openNav(){ document.getElementById("asideWrapper").style.width = "350px";
+  function openNav(){ 
+    document.getElementById("asideWrapper").style.width = "500px";
+    document.getElementById("asideWrapper").style.marginright = "10px";
+    document.getElementById("arrowSVG").style.display = "none";
   }
   
   function closeNav(){  document.getElementById("asideWrapper").style.width = "0px";
