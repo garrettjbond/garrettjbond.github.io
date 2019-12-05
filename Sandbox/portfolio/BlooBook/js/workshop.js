1f2de7db6f9cdbd08595902001f3c7770bde88c4
garrettjbond <48851694+garrettjbond@users.noreply.github.com>
2019-12-04 23:12:01 -0700

    back to normal changes

--- Sandbox/portfolio/BlooBook/js/workshop.js
+++ Sandbox/portfolio/BlooBook/js/workshop.js
@@ -6,9 +6,11 @@ function toggleSideBar(x){
     document.getElementById("asideWrapper").style.width = "500px";
     document.getElementById("asideWrapper").style.marginright = "10px";
     document.getElementById("arrowSVG").style.display = "none";
+    document.getElementsClassByName("closebtn").style.display = "";
   }
   
   function closeNav(){  
     document.getElementById("asideWrapper").style.width = "0px";
     document.getElementById("arrowSVG").style.display = "";
+    document.getElementsClassByName("closebtn").style.display = "none";
   }
