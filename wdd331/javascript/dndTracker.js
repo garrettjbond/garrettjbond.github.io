ab6978f6300bc52ade1a310d60fa45c0747b07dc
garrettjbond <48851694+garrettjbond@users.noreply.github.com>
2019-12-08 19:25:03 -0700

    fixed js error preventing push

--- wdd331/javascript/dndTracker.js
+++ wdd331/javascript/dndTracker.js
@@ -177,7 +177,7 @@ function toggleStyle(){
 //!     filter = input.value.toUpperCase();
 //!     ul = document.getElementById("myMenu");
 //!     li = ul.getElementsByTagName("li");
-  !
+//!
 //!     // Loop through all list items, and hide those who don't match the search query
 //!     for (i = 0; i < li.length; i++) {
 //!       a = li[i].getElementsByTagName("a")[0];
