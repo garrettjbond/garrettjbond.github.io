3bd7c78d8e6bf57fc7ad17a75064b7dd8621116d
garrettjbond <48851694+garrettjbond@users.noreply.github.com>
2019-12-08 19:22:56 -0700

    Search bar logic found still needed to be implemented

--- wdd331/javascript/dndTracker.js
+++ wdd331/javascript/dndTracker.js
@@ -3,7 +3,7 @@ console.log("test");
 sortItem();
 toggleStyle();
 fetchMonsters();
-// searchIt();
+//! searchIt();
 
 
 function sortItem() {
@@ -169,22 +169,22 @@ function toggleStyle(){
     }
 }
 
-//Search Bar input filtering
-// function searchIt() {
-//     // Declare variables
-//     var input, filter, ul, li, a, i;
-//     input = document.getElementById("mySearch");
-//     filter = input.value.toUpperCase();
-//     ul = document.getElementById("myMenu");
-//     li = ul.getElementsByTagName("li");
-  
-//     // Loop through all list items, and hide those who don't match the search query
-//     for (i = 0; i < li.length; i++) {
-//       a = li[i].getElementsByTagName("a")[0];
-//       if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-//         li[i].style.display = "";
-//       } else {
-//         li[i].style.display = "none";
-//       }
-//     }
-//   }
+//!Search Bar input filtering
+//! function searchIt() {
+//!     // Declare variables
+//!     var input, filter, ul, li, a, i;
+//!     input = document.getElementById("mySearch");
+//!     filter = input.value.toUpperCase();
+//!     ul = document.getElementById("myMenu");
+//!     li = ul.getElementsByTagName("li");
+  !
+//!     // Loop through all list items, and hide those who don't match the search query
+//!     for (i = 0; i < li.length; i++) {
+//!       a = li[i].getElementsByTagName("a")[0];
+//!       if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+//!         li[i].style.display = "";
+//!       } else {
+//!         li[i].style.display = "none";
+//!       }
+//!     }
+//!   }
