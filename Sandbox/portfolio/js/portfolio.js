9bec5eec5680b94d7ac2bc6192eb165bff423578
garrettjbond <48851694+garrettjbond@users.noreply.github.com>
2020-02-26 23:32:22 -0700

    button and new image

--- Sandbox/portfolio/js/portfolio.js
+++ Sandbox/portfolio/js/portfolio.js
@@ -4,3 +4,11 @@ function openNav(){ document.getElementById("mySidenav").style.width = "250px";
 
 function closeNav(){  document.getElementById("mySidenav").style.width = "0px";
 }
+
+function copyContents(){
+    var copyMe = "gbond.dev@gmail.com";
+
+    document.execCommand("copy");
+
+    alert("Email copied");
+}
