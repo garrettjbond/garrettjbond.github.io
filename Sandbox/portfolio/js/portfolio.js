392c6cf1bc9c34ab65c702d4b54ab9545f23bb23
garrettjbond <48851694+garrettjbond@users.noreply.github.com>
2020-03-04 19:10:27 -0700

    Javascript cleaning

--- Sandbox/portfolio/js/portfolio.js
+++ Sandbox/portfolio/js/portfolio.js
@@ -4,11 +4,3 @@ function openNav(){ document.getElementById("mySidenav").style.width = "250px";
 
 function closeNav(){  document.getElementById("mySidenav").style.width = "0px";
 }
-
-function copyContents(){
-    var copyMe = "gbond.dev@gmail.com";
-
-    document.execCommand("copy");
-
-    alert("Email copied");
-}
