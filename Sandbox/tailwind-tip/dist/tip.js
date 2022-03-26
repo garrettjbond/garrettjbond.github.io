d48896f2992aa995b1f8099a64d90a21f0d7dd94
Garrett Bond <gbond.dev@gmail.com>
2022-03-26 09:02:00 -0600

    Fixed footer issue

--- Sandbox/tailwind-tip/dist/tip.js
+++ Sandbox/tailwind-tip/dist/tip.js
@@ -1,4 +1,11 @@
-console.log("test connection");
+$('input').focusin(function(){
+    $('footer').hide();
+});
+$('input').focusout(function(){
+    $('footer').show();
+});
+
+
 
 function calculateTip(btnValue){
 //gets the user input for the Bill total and assigns it to a variable.
@@ -6,26 +13,22 @@ const bill = parseFloat(document.getElementById("befTotal").value);
 //changes the input to be the desired decimal place.
 const pTotal = bill.toFixed(4);
 
-console.log(pTotal)
-
 //gets the user input for the quality of service and assigns it to a variable.
 
-var service = parseFloat(btnValue.value);
-var serv = service.toFixed(4);
-console.log(serv);
+const service = parseFloat(btnValue.value);
+const serv = service.toFixed(4);
 
-var tip = pTotal * (serv / 100.00);
+const tip = pTotal * (serv / 100.00);
 tip.toFixed(2);
 const finalTip = parseFloat(tip).toFixed(2);
 const check = isNaN(pTotal);
-console.log(check + "Check")
 if(check){
     document.getElementById("result").innerHTML = "$0.00";
-    console.log("IF");
 }
 else{
     document.getElementById("result").innerHTML = "$" + finalTip;
-    console.log("ELSE");
 }
 
 }
+
+
